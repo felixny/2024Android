515edd625498ab645f094107fa63008e09c84006
felixny <felix8x419x@gmail.com>
2025-02-15 17:01:56 -0500

    4.1

--- app/ui/src/main/java/jp/co/mixi/androidtraining/ui/BookItem.kt
+++ app/ui/src/main/java/jp/co/mixi/androidtraining/ui/BookItem.kt
@@ -1,16 +1,25 @@
 package jp.co.mixi.androidtraining.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
-import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,6 +36,13 @@ fun BookItem(
         modifier = modifier.padding(horizontal= 16.dp, vertical = 8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
+        Icon(
+            painter = painterResource(id = R.drawable.book),
+            contentDescription = null,
+            modifier = Modifier.size(48.dp).clip(CircleShape).background(Color.Red).padding(8.dp),
+            tint = Color.White
+        )
+        Spacer(modifier = Modifier.width(16.dp))
         Column(
             modifier = modifier.weight(1f),
             verticalArrangement = Arrangement.spacedBy(4.dp)
@@ -44,7 +60,7 @@ fun BookItem(
         Button(
             onClick = {}
         ) {
-            Text(text = "読む")
+            Text(text = stringResource(R.string.read))
         }
     }
 
